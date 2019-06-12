1fe816dd606af9c87be62529b6501f5e2d717d6f
dongyuanxin <yuanxin.me@gmail.com>
2019-06-12 20:26:40 +0800

    add: 绑定Issue的评论插件,增加技术分享专题

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -8,5 +8,20 @@ module.exports = {
   ...baseConf,
   markdown: mdConf,
   themeConfig: themeConf,
-  plugins: []
+  plugins: [
+    [
+      'vuepress-plugin-comment',
+      {
+        choosen: 'gitalk', 
+        options: {
+          clientID: '500fe78f5be2549053a1',
+          clientSecret: '1ec36557e7e1f0e9fa81543cae888e76c169d7e4',
+          repo: 'blog',
+          owner: 'TencentCloudBase',
+          admin: ['dongyuanxin'],
+          distractionFreeMode: false 
+        }
+      }
+    ]
+  ]
 }
--- .vuepress/config/themeConf.js
+++ .vuepress/config/themeConf.js
@@ -9,9 +9,18 @@ module.exports = {
   sidebar,
   nav: [
     { text: '导航页', link: '/guide/' },
-    { text: '开发指南', link: '/开发指南/开发简介.html' },
     { text: '产品介绍', link: '/产品介绍/产品概述.html' },
-    { text: '技术分享', link: '/技术分享/'},
+    { text: '开发指南', link: '/开发指南/开发简介.html' },
+    { 
+      text: '技术分享', 
+      link: '/技术分享/',
+      items: [
+        { text: '官方教程', link: '/技术分享/官方教程/树洞小程序.html' },
+        { text: '实战分享', link: '/技术分享/实战分享/Taro商城实战.html' },
+        { text: '客户案例', link: '/技术分享/客户案例/猫眼.html' },
+        { text: '洞察', link: '/技术分享/洞察/从前端到全栈.html' },
+      ]
+    },
     {
       text: '更多',
       items: [
