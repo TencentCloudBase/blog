e92f53418bb28daa2ab8a8acc131a2d24f37f39e
dongyuanxin <yuanxin.me@gmail.com>
2019-06-19 13:37:46 +0800

    add: 开发指南目录结构

--- .vuepress/config/themeConf.js
+++ .vuepress/config/themeConf.js
@@ -9,8 +9,19 @@ module.exports = {
   sidebar,
   nav: [
     { text: '导航页', link: '/guide/' },
-    { text: '产品介绍', link: '/产品介绍/产品概述.html' },
-    { text: '开发指南', link: '/开发指南/开发简介.html' },
+    { 
+      text: '产品介绍', 
+      link: '/产品介绍/产品概述.html',
+    },
+    { 
+      text: '开发指南', 
+      items: [
+        { text: '控制台简介', link: '/开发指南/控制台简介/小程序云开发控制台.html' },
+        { text: '云数据库', link: '/开发指南/数据库/概述.html' },
+        { text: '云存储', link: '/开发指南/存储管理/概述.html' },
+        { text: '云函数', link: '/开发指南/云函数/概述.html' },
+      ]
+    },
     { 
       text: '技术分享', 
       link: '/技术分享/',
@@ -22,7 +33,7 @@ module.exports = {
       ]
     },
     {
-      text: '更多',
+      text: '了解更多',
       items: [
         { text: '团队介绍', link: '/about/' },
         { text: '更新日志', link: '/log/' },
