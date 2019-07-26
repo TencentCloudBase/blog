95bfb42380b764dd8924780a0dc9409aabbf37e7
yuanxindong <yuanxin.me@gmail.com>
2019-07-26 16:02:29 +0800

    remove: loader插件

--- .vuepress/plugins/index.js
+++ .vuepress/plugins/index.js
@@ -1,7 +1,6 @@
 module.exports = [
   require('./my-frontmatter/index.js'),
   require('./my-md5/index.js'),
-  require('./my-loader/index.js'),
   'vuepress-plugin-viewer',
   ['@vuepress/google-analytics', {ga: 'UA-124601890-2'}],
   '@vuepress/back-to-top',
